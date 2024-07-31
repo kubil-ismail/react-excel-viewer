f49f27f4c3dd160a46c55fab4b17638d1f2626ec
kubil07 <bilkis@imajin.id>
2024-07-31 14:49:59 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import ExcelViewer from "excel-viewer";
+import { useSearchParams } from "react-router-dom";
+import React from "react";
 
 function App() {
+  const [searchParams] = useSearchParams();
+  const url = searchParams.get("url");
+
+  React.useEffect(() => {
+    if (url) {
+      new ExcelViewer("#excel-view", url, { themeBtn: false });
+    }
+  }, [url]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="overlay" />
+      <div id="excel-view"></div>
     </div>
   );
 }
